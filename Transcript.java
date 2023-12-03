90a26ea7b8613db1cc2c0aa72621d74820092b6c
Darragh12345678 <150141391+Darragh12345678@users.noreply.github.com>
2023-12-03 22:43:29 +0000

    Update Transcript.java

--- Transcript.java
+++ Transcript.java
@@ -35,7 +35,7 @@ public class Transcript {
     }
 
     private double calculateOverallQCA() {
-        // Implement logic to calculate overall QCA based on all completed modules
+        // Implement a way to calculate overall QCA based on all completed modules
         // You may need to access the student's completed modules and results
         // Replace "student" with the actual reference to your Student object
         Student student = getStudent(); // Implement the logic to retrieve the Student
@@ -45,7 +45,7 @@ public class Transcript {
     }
 
     private Student getStudent() {
-        // Implement logic to retrieve the Student
+        // Implement a way to retrieve the Student
         // This might involve searching a list or querying a database
         // Return an actual Student object
         return null;
