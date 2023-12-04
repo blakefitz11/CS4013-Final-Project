bae5be4169affaa22996f98115f779639127a48d
Darragh12345678 <150141391+Darragh12345678@users.noreply.github.com>
2023-12-04 01:03:40 +0000

    Update Programme.java

--- Programme.java
+++ Programme.java
@@ -1,24 +1,40 @@
 import java.util.ArrayList;
 
-/**
- *
- */
 public class Programme {
     private String name;
     private String id;
     private int semesters;
     private ArrayList<Module> modules;
 
+/**
+     * Constructor for programme takes: name, id, semesters
+     * @param name
+     * @param id
+     * @param semesters
+     */
+    
     public Programme(String name, String id, int semesters){
         this.name = name;
         this.id = id;
         this.semesters = semesters;
         this.modules = new ArrayList<>();
     }
+
+/**
+     * Adds module
+     * @param module
+     */
+    
     public void addModule(Module module){
         modules.add(module);
     }
 
+/**
+     * Returns the ith module
+     * @param i
+     * @return
+     */
+    
     public Module getModuleFromList(int i) {
         return modules.get(i);
     }
